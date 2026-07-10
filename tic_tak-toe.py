a245eef7ccc94df8da04565bc304ef3c0a54aca1
jcarmichael <jcarmichael@basecampcoding academy.org>
2026-07-10 15:37:12 -0500

    added comments

--- tic_tak-toe.py
+++ tic_tak-toe.py
@@ -1,33 +1,31 @@
 
-from tkinter import *
 
-#window = Tk()
-#window.geometry("420x420")
-#window.title("Tik_Tac_Toe")
-#window.mainloop()
-
+#validate coordinates no matter input
 def coord_validator(board):
     while True:
         letter = None
         number = None
         num = input("Enter coords > ")
+        #general input validation
         for i in range(len(num)):
             if len(num) == 2:
                 if num[i].isdigit() and number == None:
                     number = int(num[i])
                 elif letter == None and num[i] in board:
                     letter = num[i]
+        #final input validation. prevents instances of ab or 12
         if number in range(0,4) and letter in board and board[letter][number] == "_":
             return letter, number    
         else:
             print("Invalid input")       
         
-
+#parent class of all players
 class Player:
     def __init__(self, name, winner):
         self.name = name
         self.winner = winner
-        
+
+#child of player class
 class Computer(Player):
     def __init__(self,name,winner,letter):
         super().__init__(name, winner)
@@ -35,12 +33,13 @@ class Computer(Player):
         self.winner = winner
         self.letter = letter
 
+
+#this just prints off all types of current posisions. this should be remade as a Board method later
     def x_turn2(self,board):
         print(f'x pos {self.x_pos(board)}')
         print(f'o pos {self.o_pos(board)}')
         print(f'pop pos {self.pos_pos(board)}')
-        
-
+#returns all x positions in a ["a1", "a2"] format
     def x_pos(self, board):
         x_pos = []
         for letter in board:
@@ -48,8 +47,7 @@ class Computer(Player):
                 if board[letter][number] == "x":
                     x_pos.append(f"{letter}{number}")
         return x_pos
-    
-
+#returns all o postions in a ["a1", "a2"] format
     def o_pos(self, board):
         o_pos = []
         for letter in board:
@@ -57,8 +55,7 @@ class Computer(Player):
                 if board[letter][number] == "o":
                     o_pos.append(f"{letter}{number}")
         return o_pos
-
-
+#returns all possible postions in a ["a1", "a2"] format (SHOULD BE HELPFUL FOR MAKING THE AI)
     def pos_pos(self, board):
         pos_pos = []
         for letter in board:
@@ -67,7 +64,7 @@ class Computer(Player):
                     pos_pos.append(f'{letter}{number}')
         return pos_pos
 
-
+#computer turns for if it is playing as x's or o's (currently not a feature, computer is always x's atm)
     def x_turn(self,board):
         letter, number = coord_validator(board)
         board[letter][number] = "x"
@@ -76,7 +73,7 @@ class Computer(Player):
         letter, number = coord_validator(board)
         board[letter][number] = "o"
 
-
+#child of the player class
 class Person(Player):
 
     def __init__(self, name, winner, letter):
@@ -85,6 +82,7 @@ class Person(Player):
         self.winner  = winner
         self.letter = letter
 
+#person turns for if it is playing as x's or o's (currently not a feature, person is always o's atm)
     def x_turn(self,board):
         letter, number = coord_validator(board)
         board[letter][number] = "x"
@@ -98,19 +96,28 @@ class Person(Player):
 
 class Board:
     def __init__(self):
+        #inital board in a easy to read format
         self.board = {"a":{1:"_",2:"_",3:"_"},
                       "b":{1:"_",2:"_",3:"_"},
                       "c":{1:"_",2:"_",3:"_"}}
+        
+        #list of players to be referenced for other operations
         self.players = []
 
+    #add players to the game, will need to be sure this is happening in the main program
     def add_player(self,player):
         self.players.append(player)
         
+
+    #checks win condition, this is called in the main program and is not automatically ran when a turn happens (I may change this)
     def x_win_check(self):
+
+        #winner key (every stright line adds to 15, there is no possible combination that adds to 15 and isnt a win condition)
         convert_board = {"a1":8, "a2":1, "a3":6,
                          "b1":3, "b2":5, "b3":7,
                          "c1":4, "c2":9, "c3":2}
         
+        #convert all x positions into a list of ints
         x_pos = self.players[1].x_pos(self.board)
         if len(x_pos) >= 3: 
             win_num = []
@@ -118,18 +125,26 @@ class Board:
                 if ele in convert_board:
                     win_num.append(convert_board[ele])
 
+            # check if there is a combination of 3 unique ints that add to 15
             for i in range(len(win_num)):
                 for j in range(len(win_num)):
                     for k in range(len(win_num)):
+                        #prevents indexing out of range
                         if i + 2 < len(win_num):
+                            #checks to be sure none of the numbers are of the same index value
                             if win_num[i] + win_num[j] + win_num[k] == 15 and i != j != k != i:
                                 self.players[1].winner = True
 
+
+    #checks win condition, this is called in the main program and is not automatically ran when a turn happens (I may change this)
     def o_win_check(self):
+
+        #winner key (every stright line adds to 15, there is no possible combination that adds to 15 and isnt a win condition)
         convert_board = {"a1":8, "a2":1, "a3":6,
                          "b1":3, "b2":5, "b3":7,
                          "c1":4, "c2":9, "c3":2}
         
+        #convert all o positions into a list of ints
         o_pos = self.players[1].o_pos(self.board)
         if len(o_pos) >= 3: 
             win_num = []
@@ -137,14 +152,17 @@ class Board:
                 if ele in convert_board:
                     win_num.append(convert_board[ele])
 
+            # check if there is a combination of 3 unique ints that add to 15
             for i in range(len(win_num)):
                 for j in range(len(win_num)):
                     for k in range(len(win_num)):
+                        #prevents indexing out of range
                         if i + 2 < len(win_num):
+                            #checks to be sure none of the numbers are of the same index value
                             if win_num[i] + win_num[j] + win_num[k] == 15 and i != j != k != i:
                                 self.players[0].winner = True
 
-
+    #prints updated board positions
     def print_board(self):
             print(f'  1 2 3')
             print(f'a {self.board["a"][1]} {self.board["a"][2]} {self.board["a"][3]}')
@@ -153,20 +171,22 @@ class Board:
 
 
 def main():
+    #create players
     person1 = Person("Jake", False, "o")
     computa1 = Computer("computa", False, "x")
 
+    #create board and add players to it
     board1 = Board()
     board1.add_player(person1)
     board1.add_player(computa1)
 
 
     while not person1.winner and not computa1.winner:
+
         for player in board1.players:
             board1.print_board()
             print(f"{player.name}'s turn, you are {player.letter}'s")
             if player.letter == "x":
-                 #player.x_turn2(board1.board)
                  player.x_turn(board1.board)
                  board1.x_win_check()        
                  if player.winner == True:
@@ -181,5 +201,8 @@ def main():
         for player in board1.players:
             if player.winner == True:
                 print(f'{player.name} is the winner!')
+
+
+
 if __name__ == "__main__":
     main()
